c24ea3d7b6f59a922efcd87cc58975b3c3140342
Sias Mey <siamey@gmail.com>
2023-04-20 08:26:04 +0200

    feat: add half duplex version of ergoflat_split

---
+++ keyboards/siasmey/ergoflat_split_hd/config.h
@@ -0,0 +1,31 @@
+// Copyright 2023 SiasMey (@SiasMey)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+// #define NO_ACTION_LAYER
+// #define NO_ACTION_TAPPING
+// #define NO_ACTION_ONESHOT
+#define SPLIT_USB_DETECT
+
+// #define SERIAL_USART_FULL_DUPLEX
+// #define SERIAL_USART_TX_PIN GP12
+// #define SERIAL_USART_RX_PIN GP13
+
+
+/* Reset. */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
